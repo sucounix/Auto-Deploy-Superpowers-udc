a866f1dc1387c52ff818086a62668fcea177344f
Suc0_Unix <57755186+sucounix@users.noreply.github.com>
2021-04-13 13:06:18 +0200

    prod

--- backend/src/modules/domain/employees/entities/employee.entity.ts
+++ backend/src/modules/domain/employees/entities/employee.entity.ts
@@ -1,9 +1,9 @@
-import * as moment from 'moment-timezone';
+//import * as moment from 'moment-timezone';
+const moment = require('moment');
 import { AggregateRoot } from '../../../common/entities';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { CreateEmployee } from '../commands/create-employee.command';
 import { EmployeeCreated } from '../events/employee-created.event';
-
 @Entity()
 export class Employee extends AggregateRoot<number> {
   //change <string> to <number> above to fix the compile error
@@ -32,102 +32,72 @@ export class Employee extends AggregateRoot<number> {
       this.bankName = params.bankName;
       this.birthdate = getDateFromString(params.birthdate);
     }
-
     // this.apply(new EmployeeCreated(this.id, this.firstName));
   }
-
   @PrimaryGeneratedColumn()
   //change <string> to <number> in line 8 to fix the compile error
   id: number;
-
   @Column({ length: 100 })
   public firstName: string;
-
   @Column({ length: 100, nullable: true })
   public middleName: string;
-
   @Column({ length: 100 })
   public lastName: string;
-
   @Column({ length: 100, nullable: true })
   public secondLastName: string;
-
   @Column({ length: 100, nullable: true })
   public displayName: string;
-
   @Column({ length: 50, default: '' })
   public companyEmail: string;
-
   @Column({ length: 50, nullable: true, default: '' })
   public personalEmail: string;
-
   @Column({ nullable: true })
   public birthdate: Date;
-
   @Column()
   public startDate: Date;
-
   @Column({ length: 200, nullable: true })
   public address: string;
-
   @Column({ length: 100, nullable: true })
   public phoneNumber: string;
-
   @Column({ length: 100, nullable: true })
   public bankName: string;
-
   @Column({ length: 100, nullable: true })
   public accountNumber: string;
-
   @Column({ nullable: true })
   public gender: Gender;
-
   @Column({ type: 'json', default: '{}' })
   public tags: string;
-
   @Column({ length: 100 })
   public country: string;
-
   @Column({ length: 100 })
   public region: string;
-
   @Column({ length: 100 })
   public city: string;
-
   @Column()
   public effectiveDate: Date;
-
   @Column('decimal')
   public salary: number;
-
   @Column()
   public salaryType: SalaryType;
-
   @Column({ default: true })
   public isActive: boolean;
-
   @Column({ default: 40 })
   public workingHoursPerWeek: number;
 }
-
 export enum Gender {
   MALE = 'male',
   FEMALE = 'female',
 }
-
 export enum SalaryType {
   YEARLY = 'yearly',
   HOURLY = 'hourly',
 }
-
 export function getDateFromString(date: string): Date {
-  return moment(date).format('M/D/YYYY');
+  return moment(date).format('MM-DD-YYYY');
 }
-
 function getGenderFromEnum(gender: string): Gender {
   return Gender[Object.keys(Gender).find(key => Gender[key] === gender)];
 }
-
 function getSalaryTypeFromEnum(salaryType: string): SalaryType {
   return SalaryType[
     Object.keys(SalaryType).find(key => SalaryType[key] === salaryType)
